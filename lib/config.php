8e2ba60441ccacbb8d8ec3292fc2fdca4e269e0b
verath <verath@ibepeter>
2012-09-06 17:31:00 +0200

    Don't display, but still log errors on production.

--- lib/config.php
+++ lib/config.php
@@ -12,9 +12,14 @@
     if( SHOW_DEBUG ) {
         // Full error reporting
         error_reporting(-1);
+        ini_set("display_errors", 1);
+
     } else {
-        // Turn off error reporting
-        error_reporting(0);
+        // Turn off error displaying, log instead
+        error_reporting(-1);
+        ini_set("display_errors", 0);
+        ini_set("log_errors", 1);
+
     }
 
     // Set the session cookie to HTTPOnly (_should_ not be changeable by javascript)
