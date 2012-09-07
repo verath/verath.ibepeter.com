8e51b6c04ba7ea22a77514249a86d29fcf88c8b2
verath <verath@ibepeter>
2012-09-07 12:42:55 +0200

    Always log errors

--- lib/config.php
+++ lib/config.php
@@ -13,13 +13,13 @@
         // Full error reporting
         error_reporting(-1);
         ini_set("display_errors", 1);
+        ini_set("log_errors", 1);
 
     } else {
         // Turn off error displaying, log instead
         error_reporting(-1);
         ini_set("display_errors", 0);
         ini_set("log_errors", 1);
-
     }
 
     // Set the session cookie to HTTPOnly (_should_ not be changeable by javascript)
