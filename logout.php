ba8eb3edf10966184241662349675ffdc7ede581
verath <verath@ibepeter>
2012-09-22 10:36:55 +0200

    Fixed indentation

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
-	require_once('lib/db.php');
+    require_once('lib/db.php');
     require_once('lib/User/sessionUser.class.php');
 
-	$user = new SessionUser( $pdo );
-	$user->logout();
+    $user = new SessionUser( $pdo );
+    $user->logout();
 
-	header('location: /')
+    header('location: /')
 ?>
